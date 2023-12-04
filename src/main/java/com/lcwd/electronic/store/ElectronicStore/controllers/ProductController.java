6abe9da054b2418ff4ffafee9517039eb53f9d9a
theprashantbhagat <bhagatprashant1997@gmail.com>
2023-12-04 19:22:59 +0530

    Done upto product controller update method

--- src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/ProductController.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/ProductController.java
@@ -2,8 +2,14 @@ package com.lcwd.electronic.store.ElectronicStore.controllers;
 
 import com.lcwd.electronic.store.ElectronicStore.constants.UrlConstants;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lcwd.electronic.store.ElectronicStore.dtos.ProductDto;
+import com.lcwd.electronic.store.ElectronicStore.payloads.PageableResponse;
+import com.lcwd.electronic.store.ElectronicStore.repositories.ProductRepository;
+import com.lcwd.electronic.store.ElectronicStore.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -11,4 +17,20 @@ import javax.validation.Valid;
 @RequestMapping(UrlConstants.BASE_URL)
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto){
+        ProductDto product = this.productService.createProduct(productDto);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/product")
+    public ResponseEntity<ProductDto> updateProduct(@Valid @RequestBody ProductDto productDto, @PathVariable String productId){
+        ProductDto updatedProduct = this.productService.updateProduct(productDto, productId);
+        return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
+    }
 }
