b8833032937f4597924d63f6b16f91e3467230ed
theprashantbhagat <bhagatprashant1997@gmail.com>
2023-12-21 14:25:29 +0530

    Completed Apis for cart controller

--- src/main/java/com/lcwd/electronic/store/ElectronicStore/constants/UrlConstants.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/constants/UrlConstants.java
@@ -3,4 +3,9 @@ package com.lcwd.electronic.store.ElectronicStore.constants;
 public class UrlConstants {
 
     public static final String BASE_URL="/api";
+
+    public static final String USER_BASE="/user";
+    public static final String CATEGORY_BASE="/category";
+    public static final String PRODUCT_BASE="/product";
+    public static final String CART_BASE="/cart";
 }
--- src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/CartController.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/CartController.java
@@ -1,9 +1,53 @@
 package com.lcwd.electronic.store.ElectronicStore.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lcwd.electronic.store.ElectronicStore.constants.AppConstants;
+import com.lcwd.electronic.store.ElectronicStore.constants.UrlConstants;
+import com.lcwd.electronic.store.ElectronicStore.dtos.AddItemToCartRequest;
+import com.lcwd.electronic.store.ElectronicStore.dtos.CartDto;
+import com.lcwd.electronic.store.ElectronicStore.payloads.ApiResponse;
+import com.lcwd.electronic.store.ElectronicStore.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(UrlConstants.BASE_URL+UrlConstants.CART_BASE)
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemToCartRequest request)
+    {
+        CartDto cartDto = cartService.addItemToCart(userId, request);
+        return new ResponseEntity<>(cartDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}/item/{cartItemId}")
+    public ResponseEntity<ApiResponse> removeItemFrmCart(@PathVariable String userId,@PathVariable Integer cartItemId)
+    {
+        cartService.removeItemFromCart(userId,cartItemId);
+        ApiResponse response = ApiResponse.builder()
+                .message(AppConstants.DELETE_RESPONSE).success(true).status(HttpStatus.OK).build();
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable String userId){
+
+        cartService.clearCart(userId);
+        ApiResponse response = ApiResponse.builder()
+                .message(AppConstants.DELETE_RESPONSE).success(true).status(HttpStatus.OK).build();
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<CartDto> getCartByUser(@PathVariable String userId)
+    {
+        CartDto cartDto = cartService.getCartByUser(userId);
+        return new ResponseEntity<>(cartDto,HttpStatus.OK);
+    }
+
+
 }
