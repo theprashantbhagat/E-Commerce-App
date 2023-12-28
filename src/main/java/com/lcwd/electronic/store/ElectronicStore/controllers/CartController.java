435f3028d8e483b865251f1eaae93f192cf2486b
theprashantbhagat <bhagatprashant1997@gmail.com>
2023-12-28 20:03:39 +0530

    Implemented logs in cart controller and in the service

--- src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/CartController.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/CartController.java
@@ -5,6 +5,7 @@ import com.lcwd.electronic.store.ElectronicStore.dtos.AddItemToCartRequest;
 import com.lcwd.electronic.store.ElectronicStore.dtos.CartDto;
 import com.lcwd.electronic.store.ElectronicStore.payloads.ApiResponse;
 import com.lcwd.electronic.store.ElectronicStore.services.CartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,40 +13,79 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UrlConstants.BASE_URL+UrlConstants.CART_BASE)
+@Slf4j
 public class CartController {
 
     @Autowired
     private CartService cartService;
 
+    /**
+     * @apiNote This Api is for add Item to cart
+     * @author Prashant Bhagat
+     * @since V1.0
+     * @param userId
+     * @param request
+     * @return
+     */
     @PostMapping("/{userId}")
     public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemToCartRequest request)
     {
+        log.info("Entering request for add item to cart with user id :{}",userId);
         CartDto cartDto = cartService.addItemToCart(userId, request);
+        log.info("Completed request for add item to cart with user id :{}",userId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
 
+    /**
+     * @apiNote this Api is for remove item from cart
+     * @author Prashant Bhagat
+     * @since V1.0
+     * @param userId
+     * @param cartItemId
+     * @return
+     */
     @DeleteMapping("/{userId}/item/{cartItemId}")
     public ResponseEntity<ApiResponse> removeItemFrmCart(@PathVariable String userId,@PathVariable Integer cartItemId)
     {
+        log.info("Entering request for remove item from cart with cart item id :{}",cartItemId);
         cartService.removeItemFromCart(userId,cartItemId);
         ApiResponse response = ApiResponse.builder()
                 .message(AppConstants.DELETE_CART_RESPONSE).success(true).status(HttpStatus.OK).build();
+        log.info("Completed request for remove item from cart with cart item id :{}",cartItemId);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
+    /**
+     * @apiNote this Api is for clear cart
+     * @author Prashant Bhagat
+     * @since V1.0
+     * @param userId
+     * @return
+     */
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> clearCart(@PathVariable String userId){
 
+        log.info("Entering request for clear cart with user id :{}",userId);
         cartService.clearCart(userId);
         ApiResponse response = ApiResponse.builder()
                 .message(AppConstants.DELETE_CART_RESPONSE).success(true).status(HttpStatus.OK).build();
+        log.info("Completed request for clear cart with user id :{}",userId);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
+    /**
+     * @apiNote This Api is for get cart by user
+     * @author Prashant Bhagat
+     * @since V1.0
+     * @param userId
+     * @return
+     */
     @GetMapping("/{userId}")
     public ResponseEntity<CartDto> getCartByUser(@PathVariable String userId)
     {
+        log.info("Entering request for get cart with user id :{}",userId);
         CartDto cartDto = cartService.getCartByUser(userId);
+        log.info("Completed request for get cart with user id :{}",userId);
         return new ResponseEntity<>(cartDto,HttpStatus.OK);
     }
 
--- src/main/java/com/lcwd/electronic/store/ElectronicStore/services/impl/CartServiceImpl.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/services/impl/CartServiceImpl.java
@@ -14,6 +14,7 @@ import com.lcwd.electronic.store.ElectronicStore.repositories.CartRepository;
 import com.lcwd.electronic.store.ElectronicStore.repositories.ProductRepository;
 import com.lcwd.electronic.store.ElectronicStore.repositories.UserRepository;
 import com.lcwd.electronic.store.ElectronicStore.services.CartService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class CartServiceImpl implements CartService {
 
     @Autowired
@@ -43,6 +45,7 @@ public class CartServiceImpl implements CartService {
     @Override
     public CartDto addItemToCart(String userId, AddItemToCartRequest request) {
 
+        log.info("Initiating dao call for add item to cart with user id :{}",userId);
         Integer quantity = request.getQuantity();
         String productId = request.getProductId();
         if(quantity<=0)
@@ -90,30 +93,36 @@ public class CartServiceImpl implements CartService {
         }
         cart.setUser(user);
         Cart updatedCart = cartRepository.save(cart);
+        log.info("Completed dao call for add item to cart with user id :{}",userId);
         return this.modelMapper.map(updatedCart, CartDto.class);
     }
 
     @Override
     public void removeItemFromCart(String userId, Integer cartItemId) {
 
+        log.info("Initiating dao call for remove item from cart with cart item id :{}",cartItemId);
         CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.NOT_FOUND + " id " + cartItemId));
         cartItemRepository.delete(cartItem);
-
+        log.info("Completed dao call for remove item from cart with cart item id :{}",cartItemId);
     }
 
     @Override
     public void clearCart(String userId) {
 
+        log.info("Initiating dao call for clear cart with user id :{}",userId);
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.NOT_FOUND + " id " + userId));
         Cart cart = cartRepository.findByUser(user).orElseThrow(() -> new ResourceNotFoundException(AppConstants.NOT_FOUND));
         cart.getItems().clear();
         cartRepository.save(cart);
+        log.info("Completed dao call for clear cart with user id :{}",userId);
     }
 
     @Override
     public CartDto getCartByUser(String userId) {
+        log.info("Initiating dao call for get cart with user id :{}",userId);
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.NOT_FOUND + " id " + userId));
         Cart cart = cartRepository.findByUser(user).orElseThrow(() -> new ResourceNotFoundException(AppConstants.NOT_FOUND));
+        log.info("Completed dao call for get cart with user id :{}",userId);
         return this.modelMapper.map(cart,CartDto.class);
     }
 }
